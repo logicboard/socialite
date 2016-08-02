f01223c81e172a8e0d47bfabb011a13ec5e3fac7
Michael Aldape <michaelaldape@gmail.com>
2016-08-02 10:00:35 -0500

    updated provider

--- src/Two/FitbitProvider.php
+++ src/Two/FitbitProvider.php
@@ -2,75 +2,129 @@
 
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
-class FitbitProvider extends AbstractProvider implements ProviderInterface {
+class FitbitProvider extends AbstractProvider implements ProviderInterface
+{
 
-	/**
-	 * The scopes being requested.
+    /**
+     * The scopes being requested.
      *
      * updated 1
-	 *
-	 * @var array
-	 */
+     *
+     * @var array
+     */
 
-	protected $scopes = [
-        'profile',
-        'activity'
+    protected $scopes = [
+        'activity',
+        'profile'
     ];
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function getAuthUrl($state)
-	{
-		return $this->buildAuthUrlFromBase('https://www.fitbit.com/oauth2/authorize', $state);
-	}
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopeSeparator = ' ';
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function getTokenUrl()
-	{
-		return 'https://api.fitbit.com/oauth2/token';
-	}
+    /**
+     * Format the given scopes.
+     *
+     * @param  array  $scopes
+     * @return string
+     */
+    protected function formatScopes(array $scopes)
+    {
+        return implode(' ', $scopes);
+    }
 
-	/*
-	 * {@inheritdoc}
-	 */
-	protected function getUserByToken($token)
-	{
-		$response = $this->getHttpClient()->get('https://api.fitbit.com/1/user/-/profile.json', [
-			'headers' => [
-				'Accept' => 'application/json',
-				'authorization' => 'Bearer ' . $token
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase('https://www.fitbit.com/oauth2/authorize', $state);
+    }
 
-			],
-		]);
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://api.fitbit.com/oauth2/token';
+    }
 
-		return json_decode($response->getBody(), true);
-	}
+    /**
+     * Get the access token for the given code.
+     *
+     * @param  string  $code
+     * @return string
+     */
+    public function getAccessToken($code)
+    {
+        $response = $this->getHttpClient()->post($this->getTokenUrl(), [
+            'headers' => ['Accept' => 'application/json', 'Authorization' => 'Basic ' . base64_encode($this->clientId . ':' . $this->clientSecret)],
+            'body' => $this->getTokenFields($code),
+        ]);
 
-	public function getFitnessActivities($token, $options=array())
-	{
+        return $this->parseAccessToken($response->getBody());
+    }
 
-//		$paramsString = http_build_query($options);
-//		$endPoint = 'https://www.strava.com/api/v3/activities?'.$paramsString;
-//		$response = $this->getHttpClient()->get($endPoint, [
-//			'headers' => [
-//				'Accept' => 'application/json',
-//				'authorization' => 'Bearer ' . $token
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get('https://api.fitbit.com/1/user/-/profile.json', [
+            'headers' => [
+                'Authorization' => 'Bearer '.$token,
+            ],
+        ]);
+        return json_decode($response->getBody(), true);
+    }
+
+    public function getFitnessActivities($token, $options = array())
+    {
+
+        $paramsString = http_build_query($options);
+        $endPoint = 'https://www.strava.com/api/v3/activities?'.$paramsString;
+        $response = $this->getHttpClient()->get($endPoint, [
+            'headers' => [
+                'Accept' => 'application/json',
+                'authorization' => 'Bearer ' . $token
+
+            ],
+        ]);
+
+        return json_decode($response->getBody());
+
+//        $url = $this->urlUserActivities($userId, $activityDate);
+//        $client = $this->createHttpClient();
 //
-//			],
-//		]);
+//        try {
+//            $response = $client->get($url, array(
+//                'Authorization' => $this->protocolHeader('GET', $url, $tokenCredentials),
+//            ))->send();
+//        } catch (BadResponseException $e) {
+//            $response = $e->getResponse();
+//            $body = $response->getBody();
+//            $statusCode = $response->getStatusCode();
 //
-//		return json_decode($response->getBody());
+//            throw new \Exception(
+//                "Received error [$body] with status code [$statusCode] when retrieving token credentials."
+//            );
+//        }
+//
+//        return json_decode($response->getBody());
+
+    }
+
+    public function getUserActivities($tokenCredentials, $userId, $activityDate)
+    {
 
         $url = $this->urlUserActivities($userId, $activityDate);
-        $client = $this->createHttpClient();
+        $client = $this->getHttpClient();
 
         try {
-            $response = $client->get($url, array(
-                'Authorization' => $this->protocolHeader('GET', $url, $tokenCredentials),
-            ))->send();
+            $response = $client->get($url, [
+                'headers' => ['Accept' => 'application/json', 'Authorization' => 'Basic ' . base64_encode($this->clientId . ':' . $this->clientSecret)],
+            ])->send();
         } catch (BadResponseException $e) {
             $response = $e->getResponse();
             $body = $response->getBody();
@@ -83,39 +137,54 @@ class FitbitProvider extends AbstractProvider implements ProviderInterface {
 
         return json_decode($response->getBody());
 
-	}
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAccessTokenResponse($code)
+    {
+
+        $postKey = 'form_params';
+        //(version_compare(ClientInterface::VERSION, '6') === 1) ? 'form_params' : 'body';
+        $response = $this->getHttpClient()->post($this->getTokenUrl(), [
+            'headers' => ['Accept' => 'application/json', 'Authorization' => 'Basic ' . base64_encode($this->clientId . ':' . $this->clientSecret)],
+            $postKey => $this->getTokenFields($code),
+        ]);
+        $this->credentialsResponseBody = json_decode($response->getBody(), true);
+        return json_decode($response->getBody(), true);
+    }
 
     public function urlUserActivities($userId, $dateStr)
     {
-        return 'https://api.fitbit.com/1/user/'. $userId .'/activities/date/' . $dateStr.'.json';
+        return 'https://api.fitbit.com/1/user/' . $userId . '/activities/date/' . $dateStr . '.json';
     }
 
-	/**
-	 * Get the POST fields for the token request.
-	 *
-	 * @param  string  $code
-	 * @return array
-	 */
-	protected function getTokenFields($code)
-	{
-		return array_add(
-			parent::getTokenFields($code), 'grant_type', 'authorization_code'
-  	);
-	}
+    /**
+     * Get the POST fields for the token request.
+     *
+     * @param  string $code
+     * @return array
+     */
+    protected function getTokenFields($code)
+    {
+        return array_add(
+            parent::getTokenFields($code), 'grant_type', 'authorization_code'
+        );
+    }
 
-	/**
-	 * {@inheritdoc}
-	 */
-	protected function mapUserToObject(array $user)
-	{
-		return (new User)->setRaw($user)->map([
-			'id' => $user['encodedId'],
-			'nickname' => isset($user['displayName']) ? $user['displayName'] : '',
-			'name' => isset($user['fullName']) ? $user['fullName'] : '',
-			'email' => isset($user['email']) ? $user['email'] : '',
-			'avatar' => isset($user['avatar150']) ? $user['avatar150'] : '',
-
-		]);
-	}
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'id' => $user['user']['encodedId'],
+            'nickname' => isset($user['user']['nickname']) ? $user['user']['nickname'] : '',
+            'name' => $user['user']['fullName'],
+            'email' => null,
+            'avatar' => $user['user']['avatar150'],
+        ]);
+    }
 
 }
